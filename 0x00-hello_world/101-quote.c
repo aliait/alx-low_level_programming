bd28b5585f06557925b7a477dccd70d41703df79
ali <aliaitbella3@gmail.com>
2023-03-16 18:34:50 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,14 @@
-#!/bin/bash
-gcc -S -masm=intel $CFILE -o ${CFILE/.c/.s}
+#include <unistd.h>
+
+/**
+* main - Outputs a character string to STDERR using fprintf.
+*
+* Return: Always 1
+*/
+int main(void)
+{
+
+char r[99] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, r, 59);
+return (1);
+}
