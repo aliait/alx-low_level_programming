2560091c4bd08bb5d8d5ea07cdca66a19ec022e4
ali <aliaitbella3@gmail.com>
2023-03-23 07:03:36 -0700

    0. isupper

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+int _putchar(char);
+void print_numbers(void);
+void print_most_numbers(void);
+#endif
